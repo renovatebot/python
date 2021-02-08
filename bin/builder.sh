bdcab7fa290af1ff6bfa711f79288ad62fd12e4b
Michael Kriese <michael.kriese@visualon.de>
2021-02-08 19:57:57 +0100

    chore: fix output

--- bin/builder.sh
+++ bin/builder.sh
@@ -6,8 +6,8 @@ CODENAME=$(. /etc/os-release && echo ${VERSION_CODENAME})
 
 NAME=python
 
-echo "Building ruby ${VERSION} for ${CODENAME}"
+echo "Building ${NAME} ${VERSION} for ${CODENAME}"
 python-build ${VERSION} /usr/local/${NAME}/${VERSION}
 
-echo "Compressing ruby ${VERSION} for ${CODENAME}"
+echo "Compressing ${NAME} ${VERSION} for ${CODENAME}"
 tar -cJf /cache/${NAME}-${VERSION}-${CODENAME}.tar.xz -C /usr/local/${NAME} ${VERSION}
